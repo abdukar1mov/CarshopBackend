9dff80d29927f95e2123554ed36a948699631cb5
abdukar1m0vA <101871619+abdukar1m0vA@users.noreply.github.com>
2023-10-31 07:38:04 +0500

    Backend

---
+++ src/main/java/com/carsShop/Json.java
@@ -0,0 +1,5 @@
+package com.carsShop;
+
+public class Json {
+    public String height = "wqdqwdqwdqwdqwd";
+}
---
+++ src/main/java/com/carsShop/Main.java
@@ -0,0 +1,7 @@
+package com.carsShop;
+public class Main {
+    public static void main(String[] args) {
+        Json json = new Json();
+        System.out.println(json.height);
+    }
+}
